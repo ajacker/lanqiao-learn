3885450a946cac224e154f1d11af4b149ef6af2f
ajacker <ajacker@foxmail.com>
2019-12-15 22:58:52 +0800

    完成 BEGIN-1 A+B问题

---
+++ src/begin/P1000.java
@@ -0,0 +1,17 @@
+package begin;
+
+import java.util.Scanner;
+
+/**
+ * BEGIN-1 A+B问题
+ * @author ajacker
+ * @date 2019/12/15 22:56
+ */
+public class P1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
