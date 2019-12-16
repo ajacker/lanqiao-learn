6ec7fc209bb23de3f366d00b1d29e9118318f9cb
ajacker <ajacker@foxmail.com>
2019-12-16 10:40:52 +0800

    完成 BASIC-7 特殊的数字

---
+++ src/basic/P0007.java
@@ -0,0 +1,17 @@
+package basic;
+
+
+/**
+ * BASIC-7 特殊的数字
+ *
+ * @author ajacker
+ * @date 2019/12/16 10:33
+ */
+public class P0007 {
+    public static void main(String[] args) {
+        System.out.println("153");
+        System.out.println("370");
+        System.out.println("371");
+        System.out.println("407");
+    }
+}
